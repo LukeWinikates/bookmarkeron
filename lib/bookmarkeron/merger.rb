2b2068aca159aa0e64ead63d60aa09c91764f3e2
rukednous <luke.winikates@gmail.com>
2013-08-15 22:08:48 -0700

    handle dates and ids of bookmarks

--- lib/bookmarkeron/merger.rb
+++ lib/bookmarkeron/merger.rb
@@ -18,10 +18,18 @@ module Bookmarkeron
     end
 
     def result
+      puts bookmarks_from_target
       bookmarks = bookmarks_from_target["roots"]["bookmark_bar"]["children"]
+      bookmark_next_id = bookmarks_from_source.size
       bookmarks_from_source.each do |bookmark|
         next if bookmarks.find {|existing| existing["url"] == bookmark["url"]}
-        bookmarks << bookmark
+        bookmarks << bookmark.merge({
+          "type"=> "url",
+          "date" => "13010285566708500",
+          "id" => bookmark_next_id
+        })
+
+        bookmark_next_id = bookmark_next_id + 1
       end
       bookmarks_from_target
     end
